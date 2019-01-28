36c7c4756970aabd852f240873fdf3322ce5bc26
Androho <v.peterburg@gmail.com>
2019-01-28 16:27:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<header class="flex-column">
+    <nav class="navbar  navbar-expand-md navbar-expand-sm bg-primary navbar-dark fixed-top">
+        <a class="navbar-brand ml-auto" href="#">
+            <img class="my-logo" src="img/logo1.png" alt="Logo">
+        </a>
+        <i class="navbar-brand ml-auto">Каталог продукции ТМ "БРЮС"</i>
+
+        <!-- Toggler/collapsibe Button -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <div class="ml-auto">
+                <form class="form-inline input-group-sm justify-content-center" action="#">
+                    <input class="form-control mr-sm-2 ml-auto" type="text" placeholder="Search">
+                    <button class="btn btn-info" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </div>
+            <div class="navbar-brand my-left-marging">
+                <i class=" fa fa-shopping-cart fa-3x ml-auto"></i>
+                <i class="round">0</i>
+
+            </div>
+            <div class="navbar-text my-left-marging my-right-aling ml-auto">
+                <i class="fa fa-mobile fa-lg" style="color: #ffffff">+38(095)359-90-40</i> <br>
+                <i class="fa fa-mobile fa-lg" style="color: #ffffff">+38(068)126-78-78</i>
+            </div>
+        </div>
+    </nav>
+</header>
+<!--============-->
+<main>
+
+</main>
+<!--============-->
+<footer>
+
+</footer>
+<!--============-->
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="js/jquery.js"></script>
+</body>
+</html>
